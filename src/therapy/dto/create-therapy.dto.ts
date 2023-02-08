cd2e4eff502e245a24250ae740cb139709551963
keven-correa <kturvi01@gmail.com>
2023-02-08 12:16:25 -0400

    api properties ' Swagger

--- src/therapy/dto/create-therapy.dto.ts
+++ src/therapy/dto/create-therapy.dto.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDate, IsInt, IsNotEmpty, IsOptional, MaxLength } from "class-validator";
 
 export class CreateTherapyDto {
+    @ApiProperty()
     @IsOptional()
     @MaxLength(255)
     remarks?: string
 
     @IsNotEmpty()
+    @ApiProperty()
     schedulingDate: Date
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsInt()
     therapist: number
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsInt()
     athlete: number
--- src/therapy/dto/update-therapy.dto.ts
+++ src/therapy/dto/update-therapy.dto.ts
@@ -1,22 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsInt, IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
 import { Status } from '../enums/enum.therapy'
 
 
 export class UpdateTherapyDto{
     @IsOptional()
+    @ApiProperty()
     @MaxLength(255)
     remarks?: string
 
     @IsNotEmpty()
+    @ApiProperty()
     schedulingDate: Date
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsInt()
     therapist: number
 
+    @ApiProperty()
     @IsEnum(Status)
     status: Status
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsInt()
     athlete: number
